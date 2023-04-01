87eed2575a52c6ed7f4b853f550754157e3ff30b
khumaryn <119412655+khumaryn@users.noreply.github.com>
2023-04-02 02:13:35 +0400

    Add files via upload

---
+++ WebApplication28.03homework/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication28._03homework.Controllers
+{
+    public class ProductController:Controller
+    {
+        public ContentResult GetInfo(string brand, string category, decimal price)
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "Product info - brand: " + brand + " - category: " + category + " - price:" + price;
+            return result;
+        }
+
+    }
+}
